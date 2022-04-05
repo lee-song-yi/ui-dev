d11bd4f0f6a1afea8e5162114837ed31dad5f968
lee-song-yi <okpo5071@hanmail.net>
2022-04-05 17:01:51 +0900

    popup

--- js/common.js
+++ js/common.js
@@ -28,36 +28,60 @@ function createNotification(tearget) {
     }, 3000)
 }
 
-//모달 팝업
-var targetBtn = document.querySelectorAll('.btn-modalOpen');
-var targetBtnClose = document.querySelectorAll('.btn_modalClose');
+// //모달 팝업
+// var targetBtn = document.querySelectorAll('.btn-modalOpen');
+// var targetBtnClose = document.querySelectorAll('.btn_modalClose');
 
-var targetID;
+// var targetID;
 
-// 팝업 열기
-for(var i = 0; i < targetBtn.length; i++){
-	targetBtn[i].addEventListener('click', function(e){
-	  e.preventDefault();	 
-	  targetID = this.getAttribute('href');
-	  alert(targetID);
-	  document.querySelector(targetID).classList.add('on')
-	});
-  }
+// // 팝업 열기
+// for(var i = 0; i < targetBtn.length; i++){
+// 	targetBtn[i].addEventListener('click', function(e){
+// 	  e.preventDefault();	 
+// 	  targetID = this.getAttribute('href');
+// 	  alert(targetID);
+// 	  document.querySelector(targetID).classList.add('on')
+// 	});
+//   }
 
-// 팝업 닫기
-for(var j = 0; j < targetBtnClose.length; j++){
-	targetBtnClose[j].addEventListener('click', function(e){
-		e.preventDefault();
+// // 팝업 닫기
+// for(var j = 0; j < targetBtnClose.length; j++){
+// 	targetBtnClose[j].addEventListener('click', function(e){
+// 		e.preventDefault();
 		
-		targetID = this.getAttribute('href');
-		alert(targetID);
-		document.querySelector(targetID).classList.remove('on')
-	});
-  }
+// 		targetID = this.getAttribute('href');
+// 		alert(targetID);
+// 		document.querySelector(targetID).classList.remove('on')
+// 	});
+//   }
   
 
+//모달팝업
+document.addEventListener('click', function (e) {
+    e.preventDefault();
+    var target = e.target;
 
+    if (target.hasAttribute('data-toggle') && target.getAttribute('data-toggle') == 'modal') {
+        if (target.hasAttribute('data-target')) {
+            var m_ID = target.getAttribute('data-target');
+            document.getElementById(m_ID).classList.add('on');
+            e.preventDefault();
+        }
+    }
+    
+    if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'modal') || target.classList.contains('modal')) {
+        var modal = document.querySelector('[class="modal on"]');
+        modal.classList.remove('on');
+        e.preventDefault();
+    }
+}, false);
 
+document.body.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    console.log(e);
+    document.querySelector('.btn_modalClose').click();  
+  }
+});
 
 
 
