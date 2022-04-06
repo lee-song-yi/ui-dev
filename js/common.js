2c865efb952a35560697cefc11582ac1abda64d4
lee-song-yi <okpo5071@hanmail.net>
2022-04-06 17:04:01 +0900

    commit

--- js/common.js
+++ js/common.js
@@ -28,61 +28,45 @@ function createNotification(tearget) {
     }, 3000)
 }
 
-// //모달 팝업
+// 모달 팝업
 var targetBtn = document.querySelectorAll('.btn-modalOpen');
-var targetBtnClose = document.querySelectorAll('.btn_modalClose');
-var targetModal = 
-
-var targetID;
+var targetBtnClose = document.querySelectorAll('.btn-modalClose');
 
 // 팝업 열기
 for(var i = 0; i < targetBtn.length; i++){
-	targetBtn[i].addEventListener('click', function(e){
-	  e.preventDefault();	 
-	  targetID = this.getAttribute('href');
-	  alert(targetID);
-	  document.querySelector(targetID).classList.add('on')
-	});
-  }
+    targetBtn[i].addEventListener('click', function(){	   
+        targetID = this.getAttribute('data-target');
+        //alert(targetID);
+        document.getElementById(targetID).classList.add('on');
+        createDiv();
+    });
+}
 
 // 팝업 닫기
 for(var j = 0; j < targetBtnClose.length; j++){
-	targetBtnClose[j].addEventListener('click', function(e){
-		e.preventDefault();
+    targetBtnClose[j].addEventListener('click', function(){		
 		
-		targetID = this.getAttribute('href');
-		alert(targetID);
-		document.querySelector(targetID).classList.remove('on')
-	});
-  }
-  
+        targetID = this.getAttribute('data-target');
+        //alert(targetID);
+        document.getElementById(targetID).classList.remove('on');
+        document.querySelector('.overlay').remove();
+    });
+}
 
-//모달팝업
-// document.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     var target = e.target;
+//팝업 딤
+function createDiv() {    
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('overlay');    
+    document.body.appendChild(newDiv);
+}
 
-//     if (target.hasAttribute('data-toggle') && target.getAttribute('data-toggle') == 'modal') {
-//         if (target.hasAttribute('data-target')) {
-//             var m_ID = target.getAttribute('data-target');
-//             document.getElementById(m_ID).classList.add('on');
-//             e.preventDefault();
-//         }
-//     }
-    
-//     if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'modal') || target.classList.contains('modal')) {
-//         var modal = document.querySelector('[class="modal on"]');
-//         modal.classList.remove('on');
-//         e.preventDefault();
-//     }
-// }, false);
+document.body.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && document.querySelector('.modal').style.display === 'block' ) {
+        alert('test')
+        document.querySelector('.btn-modalClose').click();
+    }
+});
 
-// document.body.addEventListener('keydown', (e) => {
-//   if (e.key === 'Escape') {
-//     console.log(e);
-//     document.querySelector('.btn_modalClose').click();  
-//   }
-// });
 
 
 
