e7569f07db3213188a2a15992849c801ca8b4e77
lee-song-yi <102194949+lee-song-yi@users.noreply.github.com>
2022-04-07 20:31:32 +0900

    리팩토링 전

--- js/common.js
+++ js/common.js
@@ -1,40 +1,12 @@
-
-
-//스크랩 툴팁
-const btnScrap = document.getElementById('btnScrap')
-const toasts = document.getElementById('toasts')
-
-const messages = [
-    '스크랩 삭제되었습니다.',
-    '스크랩 추가되었습니다.',
-]
-
-btnScrap.addEventListener('click', () => createNotification(btnScrap))
-
-function createNotification(tearget) {
-    const notif = document.createElement('div')
-    notif.classList.add('toast')
-	toasts.appendChild(notif);
-
-	if( tearget.classList.contains('on') ){		
-		notif.innerText = messages[0]
-	
-	}else{		
-		notif.innerText = messages[1]		
-	}
-
-    setTimeout(() => {
-        notif.remove()
-    }, 3000)
-}
-
 // 모달 팝업
 var targetBtn = document.querySelectorAll('.btn-modalOpen');
 var targetBtnClose = document.querySelectorAll('.btn-modalClose');
 
 // 팝업 열기
-for(var i = 0; i < targetBtn.length; i++){
-    targetBtn[i].addEventListener('click', function(){	   
+for(var i = 0; i < targetBtn.length; i++)
+{
+    targetBtn[i].addEventListener('click', function()
+    {	   
         targetID = this.getAttribute('data-target');
         //alert(targetID);
         document.getElementById(targetID).classList.add('on');
@@ -42,10 +14,12 @@ for(var i = 0; i < targetBtn.length; i++){
     });
 }
 
+
 // 팝업 닫기
 for(var j = 0; j < targetBtnClose.length; j++){
-    targetBtnClose[j].addEventListener('click', function(){		
-		
+
+    targetBtnClose[j].addEventListener('click', function()
+    {		
         targetID = this.getAttribute('data-target');
         //alert(targetID);
         document.getElementById(targetID).classList.remove('on');
